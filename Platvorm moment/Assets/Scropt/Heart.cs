b2c09eb26950d95f569952bef3db4fc14a74ce4a
Vahvl <74665243+Vahvl@users.noreply.github.com>
2021-05-02 11:38:04 +0300

    02.05
    02.05

---
+++ Platvorm moment/Assets/Scropt/Heart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Tegelaanius character = collision.GetComponent<Tegelaanius>();
+
+        if (character)
+        {
+            character.Lives++;
+            Destroy(gameObject);
+        }
+    }
+}
+
